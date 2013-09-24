9fecc7e6d3becdb622325a7a65048476e1735523
George Henderson <georgehendersonmusic@gmail.com>
2013-09-24 09:52:22 -0500

    changed client side package management to bower

--- src/www/js/app/boot.js
+++ src/www/js/app/boot.js
@@ -1,18 +1,18 @@
 require.config({
   baseUrl: 'js',
   paths: {
-    'es5-shim': 'libs/es5-shim',
-    '$': 'libs/jquery-2.0.0',
-    'jquery': 'libs/jquery-2.0.0',
-    'hammer': 'extlibs/jquery.hammer',
-    'cordova': 'libs/cordova',
-    'mout': 'libs/mout',
-    'docCookies': 'libs/docCookies',
-    'dust': 'libs/dust-full-2.0.3',
-    'dust-helpers': 'libs/dust-helpers-1.1.1',
-    'rdust': 'libs/require-dust',
-    'iscroll': 'libs/iscroll-lite',
-    'lavaca': 'lavaca'
+    'es5-shim': '../components/es5-shim/es5-shim',
+    '$': '../components/jquery/index',
+    'jquery': '../components/jquery/index',
+    'hammer': '../components/hammerjs/plugins/jquery.hammer',
+    'cordova': '../components/lavaca/src/js/libs/cordova',
+    'mout': '../components/mout/src',
+    'docCookies': '../components/lavaca/src/js/libs/docCookies',
+    'dust': '../components/dustjs-linkedin/dist/dust-full-2.0.3',
+    'dust-helpers': '../components/dustjs-linkedin-helpers/dist/dust-helpers-1.1.1',
+    'rdust': '../components/lavaca/src/js/libs/require-dust',
+    'iscroll': '../components/iscroll/dist/iscroll-lite-min',
+    'lavaca': '../components/lavaca/src/js/lavaca'
   },
   shim: {
     $: {
