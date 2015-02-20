d28949b598dd6620014cacc20ee23b0d4bfb08e3
Alex Wasner <alexwasner@Alex-Wasners-MBPR.local>
2015-02-19 22:08:51 -0600

    Oops, wrong require type

--- src/js/app/net/HomeController.jsx
+++ src/js/app/net/HomeController.jsx
@@ -1,7 +1,7 @@
 define(function(require) {
   var React = require('react');
 
-  var DashboardView = require('app/components/DashboardView.js'),
+  var DashboardView = require('jsx!app/components/DashboardView.jsx'),
       HomeView = require('jsx!app/components/HomeView.jsx'),
       DashboardViewModel = require('app/models/DashboardViewModel'),
       BaseController = require('app/net/BaseController'),
@@ -10,13 +10,11 @@ define(function(require) {
   var HomeController = BaseController.extend({
     index: function(params, history) {
       var dashboardModel = new DashboardViewModel();
-      // React.renderComponent(<DashboardView model={dashboardModel}/>, document.getElementById('view-root'));
       return this.view(HomeView, dashboardModel)
         .then(this.updateState(history, 'React!!!', params.url));
     },
     dashboard: function(params, history) {
       var dashboardModel = new DashboardViewModel();
-      // React.renderComponent(<DashboardView model={dashboardModel}/>, document.getElementById('view-root'));
       return this.view(DashboardView, dashboardModel)
         .then(this.updateState(history, 'React!!!', params.url));
     }
