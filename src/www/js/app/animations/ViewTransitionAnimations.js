a1cf90210a070862ef25fa8765ec709e95893494
David Dulak <daviddulak@me.com>
2016-03-21 11:08:19 -0500

    slide up animation fix

--- src/www/js/app/animations/ViewTransitionAnimations.js
+++ src/www/js/app/animations/ViewTransitionAnimations.js
@@ -190,42 +190,90 @@ module.exports = {
   "SLIDEUP": {
     'in': function(el){
             return new Promise(function(resolve,reject){
-              var animationClass = 'pt-page-moveFromBottom pt-page-ontop';
-              el.nextAnimationEnd(function(){
-                if (el) { el.removeClass(animationClass); }
-                return resolve();
-              })
-              .addClass(animationClass);
+              el.css({
+                'transform': 'translate3d(0,100%,0)',
+                'transition': 'all '+_commonTiming+' ease-out',
+                'z-index': '999'
+              });
+              setTimeout(function(){
+                el.css({
+                  'visibility': 'visible'
+                });
+                setTimeout(function(){
+                  el.nextTransitionEnd(function(){
+                    return resolve();
+                  });
+                  el.css({
+                    'transform': 'translate3d(0,0,0)'
+                  });
+                },1);
+              },1);
             }.bind(this));
           },
     'out': function(el){
             return new Promise(function(resolve,reject){
-              var animationClass = 'pt-page-onbottom pt-page-scaleDown';
-              el.nextAnimationEnd(function(){
-                setTimeout(function(){ if (el) { el.removeClass(animationClass); } }, 10);
-                return resolve();
-              })
-              .addClass(animationClass);
+              el.css({
+                'transform': 'translate3d(0,0,0)',
+                'transition': 'all '+_commonTiming+' ease-out',
+                'z-index': '1'
+              });
+              setTimeout(function(){
+                el.css({
+                  'visibility': 'visible'
+                });
+                setTimeout(function(){
+                  el.nextTransitionEnd(function(){
+                    return resolve();
+                  });
+                  el.css({
+                    'transform': 'translate3d(0,5%,0)'
+                  });
+                },1);
+              },1);
             }.bind(this));
           },
     'inReverse': function(el){
             return new Promise(function(resolve,reject){
-              var animationClass = 'pt-page-onbottom pt-page-scaleUp';
-              el.nextAnimationEnd(function(){
-                if (el) { el.removeClass(animationClass); }
-                return resolve();
-              })
-              .addClass(animationClass);
+              el.css({
+                'transform': 'translate3d(0,5%,0)',
+                'transition': 'all '+_commonTiming+' ease-out',
+                'z-index': '1'
+              });
+              setTimeout(function(){
+                el.css({
+                  'visibility': 'visible'
+                });
+                setTimeout(function(){
+                  el.nextTransitionEnd(function(){
+                    return resolve();
+                  });
+                  el.css({
+                    'transform': 'translate3d(0,0,0)'
+                  });
+                },1);
+              },1);
             }.bind(this));
           },
     'outReverse': function(el){
             return new Promise(function(resolve,reject){
-              var animationClass = 'pt-page-moveToBottom pt-page-ontop';
-              el.nextAnimationEnd(function(){
-                setTimeout(function(){ if (el) { el.removeClass(animationClass); } }, 10);
-                return resolve();
-              })
-              .addClass(animationClass);
+              el.css({
+                'transform': 'translate3d(0,0,0)',
+                'transition': 'all '+_commonTiming+' ease-out',
+                'z-index': '999'
+              });
+              setTimeout(function(){
+                el.css({
+                  'visibility': 'visible'
+                });
+                setTimeout(function(){
+                  el.nextTransitionEnd(function(){
+                    return resolve();
+                  });
+                  el.css({
+                    'transform': 'translate3d(0,100%,0)'
+                  });
+                },1);
+              },1);
             }.bind(this));
           }
   },
