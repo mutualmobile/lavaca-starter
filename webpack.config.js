2ee3a27f43a280440147d9cfbdf3d3236fe3bdb1
Alex Wasner <alex.wasner@gmail.com>
2016-04-05 15:22:35 -0500

    Limit filesize of fonts to prevent giant compiled css files

--- webpack.config.js
+++ webpack.config.js
@@ -52,7 +52,7 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.(jpg|png|gif|svg)$/, loader: 'url-loader' },
-      { test: /\.(woff|woff2|eot|ttf)$/, loader: 'url-loader' },
+      { test: /\.(woff|woff2|eot|ttf)$/, loader: 'url-loader?limit=10000' },
       { test: /\.less$/, loader: ExtractTextPlugin.extract('style-loader','css-loader!postcss-loader!less-loader?relativeUrls=false')
       },
       { test: /\.html$/, loader: 'dust-loader-complete', exclude: /node_modules/, query: { verbose: true } },
