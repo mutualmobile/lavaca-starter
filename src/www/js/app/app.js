326a37d546b1b9dab08d14c884bab53d91d4d720
Alex Wasner <alex.wasner@gmail.com>
2016-04-19 10:47:47 -0500

    Add dustjs helpers to app.js

--- src/www/js/app/app.js
+++ src/www/js/app/app.js
@@ -1,6 +1,7 @@
 import 'css/app.less';
 import 'imports?$=jquery!jquery-mobile';
 import 'app/env/ModernizrTests';
+import 'dustjs-helpers';
 import {History, Connectivity, Application} from 'lavaca';
 import {HomeController} from 'app/controllers/HomeController';
 import {ViewManagerViewMixin, ViewManagerViewFillin} from 'app/mixins';
