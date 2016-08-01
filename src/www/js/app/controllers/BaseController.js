882ffe33eb487388acfa1ae3e3136dd325172f03
David Dulak <daviddulak@me.com>
2016-08-01 17:11:41 -0500

    remove dependency of certain types of models

--- src/www/js/app/controllers/BaseController.js
+++ src/www/js/app/controllers/BaseController.js
@@ -1,6 +1,4 @@
 import {Controller} from 'lavaca';
-import {merge} from 'mout/object';
-import {default as stateModel} from 'app/models/StateModel';
 
 /**
  * Base controller
@@ -11,10 +9,6 @@ export let BaseController = Controller.extend(function(){
     Controller.apply(this, arguments);
   }, {
   updateState: function(historyState, title, url, stateProps){
-    var defaultStateProps = {pageTitle: title};
     this.history(historyState, title, url)();
-
-    stateProps = merge(stateProps || {}, defaultStateProps);
-    stateModel.$apply(merge(stateModel,stateProps));
   }
 });
